c2a160f3e195e4f0750b7666596fd19dec0f4e6a
Gaurav Kumar <KIT9106@KITHYD.COM>
2019-03-05 17:47:24 +0530

    backend data model change to single

--- dbActions/dbActions.js
+++ dbActions/dbActions.js
@@ -4,27 +4,47 @@ function init(client) {
   myDb = client.db('dpopApp');
 }
 
+// function addProject(data, done) {
+//   console.log(data);
+//   let project = myDb.collection('project');
+//   project.insertOne(data, function(err, result) {
+//     if (err) throw err;
+//     console.log(result.ops);
+//     done(result);
+//   })
+// }
+
 function addProject(data, done) {
-  let project = myDb.collection('project');
-  project.insertOne(data, function(err, result) {
-    if (err) throw err;
-    console.log(result.ops);
-    done(result);
-  })
+  console.log(data);
+  let project = myDb.collection('gaurav');
+  var myquery = {
+    name: "project"
+  };
+  var newvalues = { $push: { projectList:  data  } };
+  project.updateOne(myquery, newvalues,
+    function(err, result) {
+      if (err) throw err;
+      console.log(result.ops);
+      done(result);
+    });
 }
 
 function getallprojects(projectType, done) {
 
   let project = myDb.collection('project');
-  project.remove({"name":"Gaurav Kumar"}, function(err, result){
+  project.remove({
+    "name": "Gaurav Kumar"
+  }, function(err, result) {
     console.log("Deleted");
   });
-  if(projectType === "All"){
-    project.find({}).toArray(function(err, result){
+  if (projectType === "All") {
+    project.find({}).toArray(function(err, result) {
       done(result);
     });
-  }else{
-    project.find({"projectType": projectType}).toArray(function(err, result){
+  } else {
+    project.find({
+      "projectType": projectType
+    }).toArray(function(err, result) {
       done(result);
     });
   }
