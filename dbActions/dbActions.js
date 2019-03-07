76e22802d73107716f8b672583abfe40eaf04094
Gaurav Kumar <KIT9106@KITHYD.COM>
2019-03-07 12:41:34 +0530

    MongoDb Setup

--- dbActions/dbActions.js
+++ dbActions/dbActions.js
@@ -1,3 +1,4 @@
+const JSON = require('circular-json');
 let myDb = "";
 
 function init(client) {
@@ -20,7 +21,11 @@ function addProject(data, done) {
   var myquery = {
     name: "project"
   };
-  var newvalues = { $push: { projectList:  data  } };
+  var newvalues = {
+    $push: {
+      projectList: data
+    }
+  };
   project.updateOne(myquery, newvalues,
     function(err, result) {
       if (err) throw err;
@@ -29,25 +34,45 @@ function addProject(data, done) {
     });
 }
 
-function getallprojects(projectType, done) {
+// function getallprojects(projectType, done) {
+//
+//   let project = myDb.collection('project');
+//   if (projectType === "All") {
+//     project.find({}).toArray(function(err, result) {
+//       done(result);
+//     });
+//   } else {
+//     project.find({
+//       "projectType": projectType
+//     }).toArray(function(err, result) {
+//       done(result);
+//     });
+//   }
+//
+// }
 
+
+function getallprojects(projectType, done) {
   let project = myDb.collection('project');
-  project.remove({
-    "name": "Gaurav Kumar"
-  }, function(err, result) {
-    console.log("Deleted");
-  });
-  if (projectType === "All") {
-    project.find({}).toArray(function(err, result) {
-      done(result);
-    });
-  } else {
-    project.find({
-      "projectType": projectType
-    }).toArray(function(err, result) {
-      done(result);
+
+  if(projectType === "All"){
+    project.find({"name": "project"},{"projectList": 1}).toArray(function(err, result) {
+      console.log(result);
+          done(result[0].projectList);
+        });
+
+  }else{
+    project.aggregate([ { $match: { projectList: { $elemMatch: { "projectType": projectType } } } },
+     { $unwind: "$projectList" },
+      { $match: {  "projectList.projectType": projectType}},
+       { $group: { _id: "$_id", projectList: { $addToSet: "$projectList"}} }])
+       .toArray(function(err, result){
+      console.log(result[0].projectList);
+      done(result[0].projectList)
     });
   }
+
+
 
 }
 
